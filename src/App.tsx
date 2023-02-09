7e03096ced7487684ebdee8e61101a5cdb2813c8
Charles Gordinier III <cwgiii@udel.edu>
2023-02-09 15:10:48 -0500

    Charles Gordinier III

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function CharlesGordinierIIIApp(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
+                Charles Gordinier III
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
