8f2b36ea01fb51285482beaf2e4ac0eee266d92b
Charles Gordinier III <cwgiii@udel.edu>
2023-03-26 17:05:11 -0400

    State

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,33 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [num_attempts, setNumOfAttempts] = useState<number>(4);
+    const [quiz_active, setQuizInProgress] = useState<boolean>(false);
+
+    return (
+        <div>
+            <Button
+                onClick={() => [
+                    setQuizInProgress(true),
+                    setNumOfAttempts(num_attempts - 1)
+                ]}
+                disabled={quiz_active || num_attempts === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button
+                onClick={() => setQuizInProgress(false)}
+                disabled={!quiz_active}
+            >
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => setNumOfAttempts(num_attempts + 1)}
+                disabled={quiz_active}
+            >
+                Mulligan
+            </Button>
+            <span>{num_attempts}</span>
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-/**
- * Here is a helper function you *must* use to "roll" your die.
- * The function uses the builtin `random` function of the `Math`
- * module (which returns a random decimal between 0 up until 1) in order
- * to produce a random integer between 1 and 6 (inclusive).
- */
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [first_dice, setDice1] = useState<number>(2);
+    const [second_dice, setDice2] = useState<number>(1);
+
+    return (
+        <div>
+            <span data-testid="left-die">{first_dice}</span>
+            <span data-testid="right-die">{second_dice}</span>
+            <div>
+                <Button onClick={() => setDice1(d6())}>Roll Left</Button>
+                <Button onClick={() => setDice2(d6())}>Roll Right</Button>
+            </div>
+            {first_dice === second_dice && first_dice != 1 ? (
+                <span>Win</span>
+            ) : first_dice !== second_dice ? (
+                <span></span>
+            ) : (
+                <span>Lose</span>
+            )}
+        </div>
+    );
 }
